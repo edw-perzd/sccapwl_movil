bd846274440d37396215b6002b96380d59b10037
ICarlos-18 <csanchezhernandez567@gmail.com>
2024-04-08 11:20:20 -0600

    Connect bluetooth and edit personal information

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:all_bluetooth/all_bluetooth.dart';
 import 'package:flutter/material.dart';
-import 'package:sccapwl_movil/main_app.dart';
+// import 'package:sccapwl_movil/main_app.dart';
 
 // Importaciones de Firebase
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sccapwl_movil/screens/connect_bluetooth.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -11,4 +13,27 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MainApp());
+}
+
+final allBluetooth = AllBluetooth();
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder<Object>(
+        stream: allBluetooth.listenForConnection,
+        builder: (context, snapshot) {
+          final result = snapshot.data;
+          print(result);
+          return const ConnectBluetooth();
+        }
+      ),
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
